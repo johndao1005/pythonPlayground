d0f2dd655dfd5e0101938f9c1b5151092ec8da00
johndao1005 <john.d1729@gmail.com>
2021-07-21 08:55:26 +1200

    working on creating repo from folder

---
+++ Hi.py
@@ -0,0 +1,10 @@
+
+def main():
+    print("Hello World!");
+    print("World!");
+    number = input("give me number: ")
+    print(number)
+
+# line 9,10 help distinct between a stand alone program or part of a program
+if __name__ == '__main__':
+    main()
